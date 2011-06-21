532aea99e59114b6c560a81b0d86ec087dc22c0c
liu-chong <liuchong14@gmail.com>
2011-06-21 08:45:33 +0000

    完成第一阶段测试

--- src/org/opnfre/mail/Send.java
+++ src/org/opnfre/mail/Send.java
@@ -1,5 +1,155 @@
 package org.opnfre.mail;
 
-public class Send {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Scanner;
 
+import org.apache.commons.codec.binary.Base64;
+
+public class Send {
+	private List<Map<String, String>> bcc = new ArrayList<Map<String, String>>();
+
+	private String formName;
+
+	private String formMail;
+
+	private Scanner in;
+
+	private OutputStream out;
+
+	private String msg;
+
+	private Socket socket;
+
+	private String subject;
+
+	private List<Map<String, String>> to = new ArrayList<Map<String, String>>();
+
+	public void addBcc(String name, String email) {
+		Map<String, String> toInfo = new HashMap<String, String>();
+		toInfo.put(emailEnCode(name), email);
+		bcc.add(toInfo);
+	}
+
+	public void addTo(String name, String email) {
+		Map<String, String> toInfo = new HashMap<String, String>();
+		toInfo.put(emailEnCode(name), email);
+		to.add(toInfo);
+	}
+
+	public int close() throws IOException {
+		out.write("QUIT\r\n".getBytes());
+		int result = new Integer(in.nextLine().substring(0, 3));
+		socket.close();
+		return result;
+	}
+
+	public int connect() throws IOException {
+		return connect(
+				Inet4Address.getByAddress(new byte[] { (byte) 127, 0, 0, 1 }),
+				25);
+	}
+
+	public int connect(InetAddress address, int port) throws IOException {
+		socket = new Socket(address, port);
+		in = new Scanner(socket.getInputStream());
+		out = socket.getOutputStream();
+		return new Integer(in.nextLine().substring(0, 3));
+	}
+
+	private String emailEnCode(String str) {
+		String base64encode = Base64.encodeBase64String(str.getBytes());
+		return "=?UTF-8?B?" + base64encode + "?=";
+	}
+
+	private String genMailHeader() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Subject: ");
+		sb.append(subject);
+		sb.append("\r\n");
+		sb.append("From: ");
+		sb.append(formName);
+		sb.append(" <");
+		sb.append(formMail);
+		sb.append(">\r\n");
+		sb.append("To: ");
+		sb.append(genTosField(to));
+		sb.append("\r\n");
+		if (bcc.size() > 0) {
+			sb.append("Bcc: ");
+			sb.append(genTosField(bcc));
+			sb.append("\r\n");
+		}
+		sb.append("MIME-Version: 1.0\r\n");
+		sb.append("Content-type: text/html; charset=utf-8\r\n");
+		sb.append("Content-Transfer-Encoding: base64\r\n");
+		return sb.toString();
+	}
+
+	private String genTosField(List<Map<String, String>> to) {
+		StringBuilder sb = new StringBuilder();
+		boolean first = true;
+		for (Map<String, String> one : to) {
+			if (first)
+				first = false;
+			else
+				sb.append(", ");
+			Entry<String, String> toInfo = one.entrySet().iterator().next();
+			sb.append(toInfo.getKey());
+			sb.append(" <");
+			sb.append(toInfo.getValue());
+			sb.append(">");
+		}
+		return sb.toString();
+	}
+
+	public int go() throws IOException {
+		if (formMail == null)
+			return -1;
+		if (to.size() == 0)
+			return -2;
+		out.write(("MAIL FROM: <" + formMail + ">\r\n").getBytes());
+		if (new Integer(in.nextLine().substring(0, 3)) != 250)
+			return -3;
+		Entry<String, String> toInfo = to.get(0).entrySet().iterator().next();
+		out.write(("RCPT TO: <" + toInfo.getValue() + ">\r\n").getBytes());
+		if (new Integer(in.nextLine().substring(0, 3)) != 250)
+			return -4;
+		out.write("DATA\r\n".getBytes());
+		if (new Integer(in.nextLine().substring(0, 3)) != 354)
+			return -5;
+		out.write(genMailHeader().getBytes());
+		out.write("\r\n".getBytes());
+		out.write(msg.getBytes());
+		out.write("\r\n.\r\n".getBytes());
+		return new Integer(in.nextLine().substring(0, 3));
+	}
+
+	public int sayHelo(String helo) throws IOException {
+		String heloLine = "HELO " + helo + "\r\n";
+		out.write(heloLine.getBytes());
+		out.flush();
+		return new Integer(in.nextLine().substring(0, 3));
+	}
+
+	public void setFrom(String name, String email) {
+		formName = emailEnCode(name);
+		formMail = email;
+	}
+
+	public void setMessage(String msg) {
+		this.msg = Base64.encodeBase64String(msg.getBytes());
+	}
+
+	public void setSubject(String subject) {
+		this.subject = emailEnCode(subject);
+	}
 }
--- src/org/opnfre/mail/SendTest.java
+++ src/org/opnfre/mail/SendTest.java
@@ -1,15 +1,22 @@
 package org.opnfre.mail;
 
+import java.io.IOException;
+import java.util.UUID;
+
 import junit.framework.TestCase;
 
 public class SendTest extends TestCase {
-	public void testSend() {
-		Send send = new Send("51pass.org");
-		send.setSubject("Test sendmail by LcJavaMail，中文也没问题");
-		send.setFrom("liuchong <liuchong@51pass.org>");
+	public void testSend() throws IOException {
+		String uuid = UUID.randomUUID().toString();
+		Send send = new Send();
+		assertTrue(send.connect() == 220);
+		assertTrue(send.sayHelo("51pass.org") == 250);
 		send.addTo("刘冲", "liuchong14@gmail.com");
 		send.addBcc("Liu Chong", "liuchong@opnfre.org");
+		send.setFrom("liuchong", "liuchong@51pass.org");
+		send.setSubject("Test sendmail by LcJavaMail，中文" + uuid);
 		send.setMessage("<h2>Hi!</h2>\n\tTest，测试，中文");
-		send.go();
+		assertTrue(send.go() == 250);
+		assertTrue(send.close() == 221);
 	}
 }
