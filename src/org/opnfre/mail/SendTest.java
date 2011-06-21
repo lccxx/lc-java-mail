200ec7d8d22371dadb91617bede3adec143bb600
liu-chong <liuchong14@gmail.com>
2011-06-21 03:42:01 +0000

    第一阶段测试，包含中文的邮件发送测试

--- src/org/opnfre/mail/SendTest.java
+++ src/org/opnfre/mail/SendTest.java
@@ -4,6 +4,12 @@ import junit.framework.TestCase;
 
 public class SendTest extends TestCase {
 	public void testSend() {
-		
+		Send send = new Send("51pass.org");
+		send.setSubject("Test sendmail by LcJavaMail，中文也没问题");
+		send.setFrom("liuchong <liuchong@51pass.org>");
+		send.addTo("刘冲", "liuchong14@gmail.com");
+		send.addBcc("Liu Chong", "liuchong@opnfre.org");
+		send.setMessage("<h2>Hi!</h2>\n\tTest，测试，中文");
+		send.go();
 	}
 }
