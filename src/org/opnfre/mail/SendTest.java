3df4fe41b73bac986fae22ac2af14821edfcd45d
liu-chong <liuchong14@gmail.com>
2011-06-21 09:48:43 +0000

    添加 main 方法，让其在 命令行下也可以测试

--- src/org/opnfre/mail/SendTest.java
+++ src/org/opnfre/mail/SendTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.UUID;
 
 import junit.framework.TestCase;
+import junit.framework.TestSuite;
 
 public class SendTest extends TestCase {
 	public void testSend() throws IOException {
@@ -18,5 +19,11 @@ public class SendTest extends TestCase {
 		send.setMessage("<h2>Hi!</h2>\n\tTest，测试，中文");
 		assertTrue(send.go() == 250);
 		assertTrue(send.close() == 221);
+	}
+
+	public static void main(String[] args) {
+		TestSuite ts = new TestSuite();
+		ts.addTestSuite(SendTest.class);
+		junit.textui.TestRunner.run(ts);
 	}
 }
